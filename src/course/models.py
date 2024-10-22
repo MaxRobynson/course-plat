cd7212550dd930c8846a4d35f9ec82a01e32bd5c
MaxRobynson <andriamahazorobinson@gmail.com>
2024-10-22 10:31:30 +0300

    Course model almost done

--- src/course/models.py
+++ src/course/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 
-class Courses(models.Model):
+
+class AccesRequirement(models.TextChoices):
+    ANYONE = "any", "Anyone"
+    EMAIL_REQUIRED = 'email', "Email required"
+    DRAFT = 'draft', "Draft"
+
+class PublishStatus(models.TextChoices):
+    PUBLISHED = "pub", "Published"
+    COMMING_SOON = 'soon', "Comming Soon"
+    DRAFT = 'draft', "Draft"
+
+class Course(models.Model):
     title = models.CharField(max_length=140)
     description = models.TextField(blank=True, null=True)
-    #acces = models.Choices()
+    #image
+    acces = models.CharField(max_length=10,
+                             choices=AccesRequirement.choices,
+                             default=AccesRequirement.ANYONE)
+    status = models.CharField(max_length=10, 
+                              choices=PublishStatus.choices, 
+                              default=PublishStatus.DRAFT)
+    
+    @property
+    def is_published(self):
+        return self.status == PublishStatus.PUBLISHED
